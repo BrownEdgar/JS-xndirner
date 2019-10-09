61c5aacf003cc5f8d5e29e37f971de377501fccc
Edgar Nikoghosyan <nikogosjan_edgar@mail.ru>
2019-10-09 16:24:57 +0400

    add tokos in master Branch

---
+++ Tokosi hashvum/test.js
@@ -0,0 +1,20 @@
+
+function procent(a = 120, b = 50){
+	 let result = Math.floor(b/a*100);
+   console.log(`${b}-ն ${a}-ի ${result} տոկոսն է։`);
+}
+
+procent();
+
+
+function procent2(a = 120, b = 15){
+   let result = Math.floor(a/100*b);
+   console.log(`${a}-ի ${b}%-ը հավասար է  ${result}։`);
+}
+procent2();
+
+function procent3(a = 120, b = 15){
+   let result = Math.floor(a/100*b);
+   console.log(`${a}-ի ${b}%-ը հավասար է  ${result}։`);
+}
+/*feature*/
